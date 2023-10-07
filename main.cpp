e2b965aff6120ddc7725d7fa8ab65f3d141733e9
1nhki <inhki@outlook.co.id>
2023-10-07 12:10:08 +0700

    modified:   .vscode/launch.json 	modified:   .vscode/tasks.json 	new file:   Debug/mean.exe 	modified:   main.cpp 	deleted:    main/main.exe 	deleted:    main/puji.exe

--- main.cpp
+++ main.cpp
@@ -4,27 +4,34 @@
 using namespace std;
 
 int main(){
+
     int n ;
-    static double total = 0.0 ;
+    double total = 0.0 ;
     int a = 0;
     
 
+    do{
+        system("cls");
+        a = 0;
+        cout << "jumlah data :";
+            cin >> n;
+            double data[n];
 
-    cout << "jumlah data :";
-    cin >> n;
-    double data[n];
+            for (int i=0; i<n; i++){
+                cout << "Data ke -" << (i+1) <<": ";
+                cin >> data[i];
+                total += data [i];
+            }
 
-    for (int i=0; i<n; i++){
-        cout << "Data ke -" << (i+1) <<": ";
-        cin >> data[i];
-        total += data [i];
+            cout << "\nRata-Rata: "<< total / n ;
+            cout << "\ntekan 1 kemudian enter jika ingin keluar\n";
+            cin >> a;
+        } while ( a != 1 );
+
+    if (a == 1){
+        system("pause");
     }
 
-    cout << "\nRata-Rata: "<< total / n ;
-    cout << "\ntekan 1 kemudian enter jika ingin keluar\n";
-    cin >> a;
-    
-    
 
 
 
